13fdecd4005b0eaa8c4c3344053aaaf3bc11c651
Low-Frequency <94133096+Low-Frequency@users.noreply.github.com>
2022-02-02 18:44:09 +0100

    Implemented log rotation

--- klipper_config_git_backup.sh
+++ klipper_config_git_backup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 DATE=$(date +%F)
+DEL=$((($(date '+%s') - $(date -d '6 months ago' '+%s')) / 86400))
 
 sleep 15
 touch /home/pi/git_log/"$DATE"
-echo "Adding SSH key to agent" >> /home/pi/git_log/"$DATE"
-eval "$(ssh-agent -s)" >> /home/pi/git_log/"$DATE"
-ssh-add /home/pi/.ssh/github_id_rsa >> /home/pi/git_log/"$DATE"
-echo "Adding changes to push" >> /home/pi/git_log/"$DATE"
+echo "Adding SSH key to agent" | tee -a /home/pi/git_log/"$DATE"
+eval "$(ssh-agent -s)" | tee -a /home/pi/git_log/"$DATE"
+ssh-add /home/pi/.ssh/github_id_rsa | tee -a /home/pi/git_log/"$DATE"
+echo "Adding changes to push" | tee -a /home/pi/git_log/"$DATE"
 git -C /home/pi/klipper_config add .
-echo "Committing to GitHub repository" >> /home/pi/git_log/"$DATE"
-git -C /home/pi/klipper_config commit -m "backup $DATE" >> /home/pi/git_log/"$DATE"
-echo "Pushing" >> /home/pi/git_log/"$DATE"
-git -C /home/pi/klipper_config push -u origin master >> /home/pi/git_log/"$DATE"
+echo "Committing to GitHub repository" | tee -a /home/pi/git_log/"$DATE"
+git -C /home/pi/klipper_config commit -m "backup $DATE" | tee -a /home/pi/git_log/"$DATE"
+echo "Pushing" | tee -a /home/pi/git_log/"$DATE"
+git -C /home/pi/klipper_config push -u origin master | tee -a /home/pi/git_log/"$DATE"
+find /home/pi/git_log -mindepth 1 -mtime +$DEL -delete
