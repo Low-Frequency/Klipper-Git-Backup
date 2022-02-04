8a5b9f7f0b9534709f9dbe31c28f4d65281e4864
Low-Frequency <94133096+Low-Frequency@users.noreply.github.com>
2022-02-04 21:41:11 +0100

    Rewritten the restore functionality

--- restore_config.sh
+++ restore_config.sh
@@ -1,32 +1,59 @@
 #!/bin/bash
 
-ARG=$1
-URL=$2
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NONE='\033[0m'
 
-if [[ -z $ARG ]]
+echo "Which restoring mode dou you want?"
+echo "1: Restore an old config to an existing installation"
+echo "2: Restore a config to a new installation"
+echo ""
+
+read -p 'Please enter the restore mode [1|2] ' MODE
+
+if [ $MODE = 1 ]
 then
-        echo "Please provide a restore mehtod"
-        echo "Valid methods are:"
-        echo "new: New install of klipper without a git repo set up"
-        echo "restore: Restoring an old configuration into an existing git repo"
-        echo ""
-        echo "If you choose to restore to a new klipper install, please provide the URL to your git repo"
-else
-        if [ "$ARG" = "restore" ]
+        echo -e "${YELLOW}WARNING!${NONE} This will delete your current configuration!"
+        read -p 'Continue? [y|n] ' CONTINUE
+        if [ "$CONTINUE" = "y" ]
         then
                 echo "Restoring old configuration"
-                git -C /home/pi/klipper_config fetch -all
+                git -C /home/pi/klipper_config fetch --all
                 git -C /home/pi/klipper_config reset --hard origin/master
-        elif [ "$ARG" = "new" ]
+        else
+                echo -e "${RED}Restore canceled${NONE}"
+        fi
+elif [ $MODE = 2 ]
+then
+        echo "Checking SSH key"
+        if [[ -f /home/pi/.ssh/github_id_rsa ]]
         then
-                if [[ $URL == *"github.com"* ]]
+                if [[ -d /home/pi/klipper_config/.git ]]
                 then
-                        echo "Cloning from $URL"
-                        git -C /home/pi clone "$URL"
+                        echo -e "${RED}ERROR!${NONE} The klipper_config folder is already a git repository"
+                        echo "Please use restore mode 1"
                 else
-                        echo "Please provide a valid URL to a git repo"
+                        read -p 'Please provide your GitHub Username: ' USER
+                        read -p 'Please provide the name of the repo you want to restore: ' REPO
+                        URL="https://github.com/$USER/$REPO"
+
+                        echo "Backing up the default klipper_config folder"
+                        mv /home/pi/klipper_config /home/pi/klipper_config_bak
+
+                        echo "Cloning the repo"
+                        git -C /home/pi clone "$URL"
+                        mv "/home/pi/$REPO" /home/pi/klipper_config
+
+                        read -p 'Do you want to keep the old folder? [y|n] ' DEL
+                        if [ "$DEL" = "n" ]
+                        then
+                                echo "Deleting backup"
+                                rm -r /home/pi/klipper_config_bak
+                        else
+                                echo "Old folder is located at /home/pi/klipper_config_bak"
+                        fi
                 fi
         else
-                echo "Please provide valid restore method"
+                echo -e "${RED}ERROR!${NONE} Please set up a SSH key pair"
         fi
 fi
