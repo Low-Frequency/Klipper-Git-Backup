e04e9c56f939042a04f02927a8c4d6256c8551ff
Low-Frequency <94133096+Low-Frequency@users.noreply.github.com>
2022-02-04 20:06:24 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+read -p 'Please enter your GitHub Username: ' USER
+read -p 'Please enter the name of your GitHub repository: ' REPO
+read -p 'Please enter the e-mail of your GitHub account: ' MAIL
+
+URL="https://github.com/$USER/$REPO"
+
+echo "Installing git"
+sudo apt install git -y
+
+echo "Crating folders"
+mkdir /home/pi/scripts
+mkdir /home/pi/git_log
+mkdir /home/pi/.ssh
+
+echo "Generating SSH key pair"
+ssh-keygen -t ed25519 -C "$MAIL" -f /home/pi/.ssh/github_id_rsa -q -N ""
+echo "IdentityFile ~/.ssh/github_id_rsa" >> /home/pi/.ssh/config
+chmod 600 ~/.ssh/config
+
+echo "Initializing repo"
+git -C /home/pi/klipper_config init
+git -C /home/pi/klipper_config remote add origin "$URL"
+git -C /home/pi/klipper_config remote set-url origin git@github.com:"$USER"/"$REPO".git
+
+echo "Setting username"
+git config --global user.email "$MAIL"
+git config --global user.name "$USER"
+
+echo "Cloning backup script"
+git -C /home/pi/scripts clone https://github.com/Low-Frequency/klipper_backup_script
+chmod +x ~/scripts/klipper_backup_script/klipper_config_git_backup.sh
+
+echo "Setting up the service"
+sudo mv ~/scripts/klipper_backup_script/gitbackup.service /etc/systemd/system/gitbackup.service
+sudo systemctl enable gitbackup.service
+sudo systemctl start gitbackup.service
+
+echo "Almost done"
+echo "Please copy the public key and add it to your GitHub account"
+echo "You can find instructions for this here:"
+echo "https://github.com/Low-Frequency/klipper_backup_script"
+echo ""
+echo ""
+cat ~/.ssh/github_id_rsa.pub
+echo ""
+echo ""
+
+read -p "When you're done, press enter to continue " CONTINUE
+echo "Testing SSH connention"
+ssh -T git@github.com
+echo "Pushing the first backup to your repo"
+/home/pi/scripts/klipper_backup_script/klipper_config_git_backup.sh
