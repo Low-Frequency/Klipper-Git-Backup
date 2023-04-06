8649a117e642c05cdf4d6916dfe315a2fc1df763
Matthias Nowak <matthias.nowak@fadeout-rock.de>
2023-04-06 20:22:38 +0200

    Removed old version

--- backup.sh
+++ backup.sh
@@ -2,8 +2,18 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit; pwd -P )"
 
-source "${HOME}/.config/klipper_backup_script/backup.cfg"
-source "${SCRIPTPATH}/lib/backup_functions.sh"
+source "${SCRIPTPATH}/lib/functions/general_functions.sh"
+source "${SCRIPTPATH}/lib/functions/github_functions.sh"
 
-check_time
-backup_config
+if [[ -f "${HOME}/.config/klipper_backup_script/backup.cfg" ]]
+then
+  source "${HOME}/.config/klipper_backup_script/backup.cfg"
+else
+  log_msg "No config file found!"
+  exit 1
+fi
+
+init_schedule
+backup
+
+exit 0
